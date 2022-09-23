91c1272f274491fe488c84c3b13dfff61074de38
5j9 <5j9@users.noreply.github.com>
2022-09-23 11:51:32 +0330

    use xpath instead of regex

--- aggregator.py
+++ aggregator.py
@@ -1,13 +1,19 @@
 from json import loads, dumps
+from functools import partial
+from asyncio import run, gather
 
-from httpx import Client
-from regex import finditer
+from aiohttp import ClientSession
+from lxml import etree
 from path import Path
 
-client = Client()
+
+slug_table = str.maketrans(r'\/:*?"<>|', '-' * 9)
 
 project = Path(__file__).parent
 inbox = project / 'inbox'
+
+parse_html = partial(etree.fromstring, parser=etree.HTMLParser())
+parse_xml = etree.fromstring
 
 
 def load(path: Path):
@@ -20,35 +26,52 @@ url_format = """\
 URL={link}
 """.format
 
-sources = load(project / 'sources.json')
+config = load(project / 'config.json')
 
 last_links_path = project / 'last_links.json'
 last_links = load(last_links_path)
 
-for source in sources:
-    url = source['url']
-    response = client.get(url)
+
+async def check(sub):
+    url = sub['url']
+    content = await (await client.get(url, ssl=sub.get('ssl'))).read()
+
+    if sub['doctype'] == 'xml':
+        xp = parse_xml(content).xpath
+    else:
+        xp = parse_html(content).xpath
+
     url_last_links = last_links.setdefault(url, {})
 
-    for pattern in source['patterns']:
-        last_link = url_last_links.get(pattern)
+    for links_xp, titles_xp in sub['link_title_xpaths']:
+        last_link = url_last_links.get(links_xp)
         last_list_was_updated = False
 
-        for m in finditer(pattern, response.text):
-            link = m['link']
+        for link, title in zip(xp(links_xp), xp(titles_xp)):
             if link == last_link:
                 break
-
-            title = m['title'].strip()
+            if link[0] == '/':  # relative link
+                link = url.rstrip('/') + link
+            title = title.strip().translate(slug_table)
             with open(inbox / f'{title}.URL', 'w', encoding='utf8') as f:
                 f.write(url_format(link=link))
 
             if last_list_was_updated is False:
-                url_last_links[pattern] = link
+                url_last_links[links_xp] = link
                 last_list_was_updated = True
         else:
             if last_list_was_updated is False:
-                print(f'no match: {url=} {pattern=}')
+                print(f'no match: {url=} {links_xp=}')
+
+
+async def check_all():
+    # noinspection PyGlobalUndefined
+    global client
+    async with ClientSession() as client:
+        await gather(*[check(sub) for sub in config['subscriptions']])
+
+
+run(check_all())
 
 
 with last_links_path.open('w', encoding='utf8') as f:
