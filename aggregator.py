a036df9ac42215ef13783d63e9e1c02b690b2048
5j9 <5j9@users.noreply.github.com>
2023-01-07 07:08:27 +0330

    feat: create a server for managing read items

--- aggregator.py
+++ aggregator.py
@@ -1,13 +1,31 @@
 import sys
 from json import loads, dumps
 from functools import partial
-from asyncio import run, gather, TimeoutError
+from asyncio import gather, TimeoutError
 from urllib.parse import urljoin
+from atexit import register
 
 from aiohttp import ClientSession, ClientTimeout
 from aiohttp.client_exceptions import ClientConnectorError
 from lxml import etree
 from path import Path
+
+
+def get_logger():
+    import logging
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(pathname)s:%(lineno)d %(levelname)s %(message)s'
+    )
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+logger = get_logger()
 
 
 def show_exception_and_confirm_exit(exc_type, exc_value, tb):
@@ -17,10 +35,18 @@ def show_exception_and_confirm_exit(exc_type, exc_value, tb):
     raise SystemExit
 
 
-def to_html(href_texts: list[tuple[str, str]]) -> str:
-    return '<ol>' + '\n'.join([
-        f'<li><a href="{href}">{text}</a></li>' for href, text in href_texts
-    ]) + '</ol>'
+# language=html
+item_template = """\
+<div class="item">
+    <a href="{url}">{title}</a>
+    <div class='main_url'>{main_url}</div>
+    <button hx-get="/mark_as_read?main_url={main_url}&url={url}" hx-swap="delete" hx-target="closest .item">mark as read</button>
+</div>
+"""
+
+
+def create_item(url: str, title: str, main_url: str) -> str:
+    return item_template.format(url=url, title=title, main_url=main_url)
 
 
 sys.excepthook = show_exception_and_confirm_exit
@@ -42,6 +68,7 @@ def load_json(path: Path):
 
 LAST_CHECK_RESULTS_PATH = PROJECT / 'last_check_results.json'
 LAST_CHECK_RESULTS = load_json(LAST_CHECK_RESULTS_PATH)
+
 CONFIG_PATH = PROJECT / 'config.json'
 CONFIG = load_json(CONFIG_PATH)
 
@@ -57,6 +84,9 @@ def save_json(path: Path, data: dict):
                 indent='\t',
             )
         )
+
+
+register(save_json, LAST_CHECK_RESULTS_PATH, LAST_CHECK_RESULTS)
 
 
 def writefile(domain: str, content: str):
@@ -78,10 +108,10 @@ async def get_text(url, ssl):
     try:
         response = await CLIENT.get(url, ssl=ssl)
     except ClientConnectorError:
-        input(f'ClientConnectorError on {url}.\nPress enter to continue.')
+        logger.warning(f'ClientConnectorError on {url}.')
         return
     except TimeoutError:
-        input(f'TimeoutError on {url}.\nPress enter to continue.')
+        logger.warning(f'TimeoutError on {url}.')
         return
 
     return await response.text()
@@ -96,9 +126,11 @@ def get_checked_links(url):
 
 async def check(sub):
     main_url: str = sub['url']
+    logger.debug('checking %s', main_url)
 
     text = await get_text(main_url, sub.get('ssl'))
     if text is None:
+        logger.error('text is None for %s', main_url)
         return
 
     if sub['doctype'] == 'xml':
@@ -106,56 +138,41 @@ async def check(sub):
     else:
         xp = parse_html(text).xpath
 
-    for xpath in sub['xpaths']:
-        links_xp = xpath['links']
-        titles_xp = xpath['titles']
+    last_checked = LAST_CHECK_RESULTS.setdefault(main_url, {})
 
-        links = xp(links_xp)
-        if not links:
-            print(f'no match: {main_url=} {links_xp=}')
+    xpath = sub['xpaths']
+    links_xp = xpath['links']
+    titles_xp = xpath['titles']
 
-        # convert relative links to absolute
-        urls = [urljoin(main_url, link) for link in links]
-        checked_links = get_checked_links(main_url)
+    links = xp(links_xp)
 
-        new_links: list[tuple[str, str]] = []
+    if not links:
+        logger.warning(f'no match: {main_url=} {links_xp=}')
+        return
 
-        for url, title in zip(urls, xp(titles_xp)):
-            if url in checked_links:
-                break
-            new_links.append((url, title.strip()))
+    # convert relative links to absolute
+    urls = {urljoin(main_url, link): False for link in links}
 
-        if not new_links:
-            return
+    # delete old urls that no longer exist
+    for k in (last_checked.keys() - urls.keys()):
+        del last_checked[k]
 
-        domain = main_url.partition('://')[2].partition('/')[0]
-        writefile(domain, to_html(new_links))
+    items = []
+    for url, title in zip(urls, xp(titles_xp)):
+        if last_checked.setdefault(url, False):
+            break
+        items.append(create_item(url, title.strip(), main_url))
 
-        LAST_CHECK_RESULTS[main_url] = urls
+    if items:
+        logger.debug('found new items on %s', main_url)
+    else:
+        logger.debug('no new items on %s', main_url)
+    return items
 
 
 async def check_all():
     # noinspection PyGlobalUndefined
     global CLIENT
     async with ClientSession(timeout=ClientTimeout(10)) as CLIENT:
-        await gather(*[check(sub) for sub in CONFIG['subscriptions']])
-
-
-def open_inbox():
-    files = INBOX.files()
-    if not files:
-        return
-
-    import webbrowser
-
-    # more than one directory in INBOX
-    webbrowser.open(INBOX)
-
-
-run(check_all())
-
-
-# save_json(CONFIG_PATH, CONFIG)
-save_json(LAST_CHECK_RESULTS_PATH, LAST_CHECK_RESULTS)
-
-open_inbox()
+        results = await gather(*[check(sub) for sub in CONFIG['subscriptions']])
+    return [items for r in results for items in r if r is not None]
---
+++ server.py
@@ -0,0 +1,51 @@
+from aiohttp import web, WSMsgType
+
+from aggregator import logger, check_all, LAST_CHECK_RESULTS
+
+
+async def htmx(request):
+    with open('htmx.min-1.8.4.js') as f:
+        text = f.read()
+    return web.Response(text=text, content_type='text/javascript')
+
+
+async def root(request):
+    with open('inbox.html') as f:
+        inbox = f.read()
+    items = await check_all()
+    if items:
+        items_html = '\n'.join(items)
+    else:
+        items_html = 'Everything read!'
+
+    inbox = inbox.replace('<div id="items">', '<div id="items">' + items_html)
+    return web.Response(text=inbox, content_type='text/html')
+
+
+async def mark_as_read(request):
+    q = request.query
+    logger.debug('marking %s as read', q['url'])
+    LAST_CHECK_RESULTS[q['main_url']][q['url']] = True
+    return web.Response(text='', content_type='text/html')
+
+
+app = web.Application()
+app.add_routes([
+    web.get('/htmx', htmx),
+    web.get('/', root),
+    web.get('/mark_as_read', mark_as_read),
+])
+
+
+def run():
+    import webbrowser
+    webbrowser.open(f'http://localhost:{port}/')
+    web.run_app(app, host=host, port=port)
+
+
+host = '127.0.0.1'
+port = 8080
+
+
+if __name__ == '__main__':
+    run()
