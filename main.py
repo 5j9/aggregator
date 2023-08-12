2a64b1d4266c30b781026cc2eb6b50bc74955d36
5j9 <5j9@users.noreply.github.com>
2023-08-12 06:03:47 +0330

    fix(app): add route for /htmx_ws

--- main.py
+++ main.py
@@ -8,17 +8,22 @@ from collector import (
     save_json,
 )
 
+rt = web.RouteTableDef()
 
+
+@rt.get('/htmx')
 async def htmx(request):
     logger.debug('htmx')
     return web.FileResponse('htmx-1.9.4.js')
 
 
+@rt.get('/htmx_ws')
 async def htmx_ws(request):
     logger.debug('htmx_ws')
     return web.FileResponse('htmx-ws-1.9.4.js')
 
 
+@rt.get('/items')
 async def items(request):
     logger.debug('items')
 
@@ -30,18 +35,22 @@ async def items(request):
         s = f'<div id="items" hx-swap-oob="beforeend">{items_html}<div>'
         await ws.send_str(s)
 
-    await ws.send_str(f'<div id="items" hx-swap-oob="beforeend">All items checked.<div>')
+    await ws.send_str(
+        f'<div id="items" hx-swap-oob="beforeend">All items checked.<div>'
+    )
     async for msg in ws:
         ...  # todo
 
     return ws
 
 
+@rt.get('/')
 async def inbox(request):
     logger.debug('inbox')
     return web.FileResponse('inbox.html')
 
 
+@rt.get('/mark_as_read')
 async def mark_as_read(request):
     q = request.query
     logger.debug('marking %s as read', q['url'])
@@ -51,16 +60,12 @@ async def mark_as_read(request):
 
 
 app = web.Application()
-app.add_routes([
-    web.get('/htmx', htmx),
-    web.get('/items', items),
-    web.get('/', inbox),
-    web.get('/mark_as_read', mark_as_read),
-])
+app.add_routes(rt)
 
 
 def run():
     import webbrowser
+
     webbrowser.open(f'http://localhost:{port}/')
     web.run_app(app, host=host, port=port)
 
