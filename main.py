ba7a590c29ef30ec129256a458972da0c8008564
5j9 <5j9@users.noreply.github.com>
2024-04-19 10:13:16 +0330

    chore: add type hints

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from aiohttp import web
+from aiohttp.web import (
+    Application,
+    FileResponse,
+    Request,
+    Response,
+    RouteTableDef,
+    WebSocketResponse,
+    run_app,
+)
 
 from collector import (
     LAST_CHECK_RESULTS,
@@ -8,26 +16,26 @@ from collector import (
     save_json,
 )
 
-rt = web.RouteTableDef()
+rt = RouteTableDef()
 
 
 @rt.get('/htmx')
-async def htmx(request):
+async def htmx(_: Request) -> FileResponse:
     logger.debug('htmx')
-    return web.FileResponse('htmx-1.9.4.js')
+    return FileResponse('htmx-1.9.4.js')
 
 
 @rt.get('/htmx_ws')
-async def htmx_ws(request):
+async def htmx_ws(_: Request) -> FileResponse:
     logger.debug('htmx_ws')
-    return web.FileResponse('htmx-ws-1.9.4.js')
+    return FileResponse('htmx-ws-1.9.4.js')
 
 
 @rt.get('/items')
-async def items(request):
+async def items(request: Request) -> WebSocketResponse:
     logger.debug('items')
 
-    ws = web.WebSocketResponse()
+    ws = WebSocketResponse()
     await ws.prepare(request)
 
     async for items in check_all():
@@ -46,22 +54,22 @@ async def items(request):
 
 
 @rt.get('/')
-async def inbox(request):
+async def inbox(_: Request) -> FileResponse:
     logger.debug('inbox')
-    return web.FileResponse('inbox.html')
+    return FileResponse('inbox.html')
 
 
 @rt.get('/mark_as_read')
-async def mark_as_read(request):
+async def mark_as_read(request) -> Response:
     q = request.query
     logger.debug('marking %s as read', q['url'])
     LAST_CHECK_RESULTS[q['main_url']][q['url']] = True
     save_json(LAST_CHECK_RESULTS_PATH, LAST_CHECK_RESULTS)
-    return web.Response(text='', content_type='text/html')
+    return Response(text='', content_type='text/html')
 
 
 @rt.get('/mark_all_as_read')
-async def mark_all_as_read(request):
+async def mark_all_as_read(_: Request) -> Response:
     logger.info('marking all as read')
     for main_url, d in LAST_CHECK_RESULTS.items():
         for url, is_read in d.items():
@@ -69,10 +77,10 @@ async def mark_all_as_read(request):
                 logger.debug('marking %s as read', url)
                 d[url] = True
     save_json(LAST_CHECK_RESULTS_PATH, LAST_CHECK_RESULTS)
-    return web.Response(text='', content_type='text/html')
+    return Response(text='', content_type='text/html')
 
 
-app = web.Application()
+app = Application()
 app.add_routes(rt)
 
 
@@ -80,7 +88,7 @@ def run():
     import webbrowser
 
     webbrowser.open(f'http://localhost:{port}/')
-    web.run_app(app, host=host, port=port)
+    run_app(app, host=host, port=port)
 
 
 host = '127.0.0.1'
